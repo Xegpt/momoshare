fe8eeaac26140c27f5a6c082cbecb7f7a64497f8
xegpt <76429028+Xegpt@users.noreply.github.com>
2023-02-09 09:24:30 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -19,7 +19,10 @@ const MMSLS = [
 =====================请在下面编辑=====================
  */
 const MMSLS = [
-
+    
+    "https://www.maimemo.com/share/page?uid=4534968&pid=3b8dfd5cd7c9a2f77a83726e1e1372f9&tid=4ace738c709ed159f03924963e17bd0b",
+    "https://www.maimemo.com/share/page?uid=4534968&pid=3b8dfd5cd7c9a2f77a83726e1e1372f9&tid=4ace738c709ed159f03924963e17bd0b",
+    "https://www.maimemo.com/share/page?uid=4534968&pid=3b8dfd5cd7c9a2f77a83726e1e1372f9&tid=4ace738c709ed159f03924963e17bd0b",
   ]
   /**
    * 生成随机数字
