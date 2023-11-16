92d4208ba809bf27d2d6c2c2c1f053c0b2d7502b
xegpt <76429028+Xegpt@users.noreply.github.com>
2023-11-16 13:50:23 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -22,7 +22,7 @@ const MMSLS = [
     
     "https://www.maimemo.com/share/page?uid=4534968&pid=ecb08be95727e206e629213092e90262&tid=16386877e82de8192e48f461db1ad013",
     "https://www.maimemo.com/share/page?uid=4534968&pid=c715ca1d11e18bb9088a8cc495f61933&tid=f9e5d571edc497f7ed194ae5868f050f",
-    "https://www.maimemo.com/share/page?uid=4534968&pid=a373cc19647c05c02040fbd0f49b04b9&tid=ba728e480e19133ef0697fb47eb10e4f",
+    "https://www.maimemo.com/share/page?uid=4534968&pid=52cb8236afb069327bd817ad374c0040&tid=c40fa409f60f6e6efac3fa834256b289",
   ]
   /**
    * 生成随机数字
