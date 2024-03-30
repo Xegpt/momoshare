a3d83e0bf1714fa28eb3eb30d61c2f49450a1fd7
xegpt <76429028+Xegpt@users.noreply.github.com>
2024-03-30 23:20:38 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=4534968&pid=2ffa64518d9d179594e3a779b52eb850&tid=5e53fb070006312bb965a7e71df9a182",
+    "https://www.maimemo.com/share/page?uid=4534968&pid=01c723220cf8d9e859f6239a2c9c94f3&tid=e31bf592347b46e0143d2d0f152ab6c0",
     "https://www.maimemo.com/share/page?uid=4534968&pid=a3356d0e563c96c1e2ca01303fe70983&tid=1f245532cccb68733e881daa1a08657f",
     "https://www.maimemo.com/share/page?uid=4534968&pid=779b830e517c2506c44f70919b75a457&tid=9dfc214e031eeb715df482913e0cac6b",
   ]
